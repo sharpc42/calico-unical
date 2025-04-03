e7fbabd50eec8e5465a513c6d97674992f172c13
sharpc42 <chris.m.sharp@gmail.com>
2025-04-03 15:37:58 -0700

    Cost function neg, gains, flip res, Powell

--- calico/cost_function_calculations.py
+++ calico/cost_function_calculations.py
@@ -1149,8 +1149,8 @@ def cost_unical(
     """
 
     gains_expanded = (gains[ant1_inds] * np.conj(gains[ant2_inds]))[np.newaxis, :]
-    res_vec_1 = gains_expanded * fit_vis - data_vis
-    res_vec_2 = model_vis - fit_vis
+    res_vec_1 = data_vis - gains_expanded * fit_vis
+    res_vec_2 = fit_vis - model_vis
     cost = -np.sum(vis_weights * np.abs(res_vec_1) ** 2) + np.sum(model_weights * np.abs(res_vec_2)**2)
 
     # if lambda_val > 0:
